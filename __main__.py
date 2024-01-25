bbe4ed574720ac8c13e77ae512f8f1b8cb1422c5
MoRanYue <2627706725@qq.com>
2024-01-25 23:21:40 +0800

    the author can be set with command argument

--- __main__.py
+++ __main__.py
@@ -1,9 +1,22 @@
 import asyncio
+import sys
 from bandcamp import Bandcamp
 
 async def main():
+  author = None
+  try:
+    author = sys.argv[1].strip()
+    print(f"Start to searching for {author}'s songs.")
+  except:
+    print("The author has not been set.")
+    return
+  
+  if not author or author == "":
+    print("The author has not been set.")
+    return
+
   bandcamp = Bandcamp()
-  await bandcamp.addAllAlbums("slowedandreverb")
+  await bandcamp.addAllAlbums(author)
   await bandcamp.downloadAlbums()
 
 if __name__ == '__main__':
